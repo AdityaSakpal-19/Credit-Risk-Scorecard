7fd15a0b6f4c6fd8fa8a9aad5fbf0d1a139b9d23
AdityaSakpal-19 <adityasakpal205@gmail.com>
2026-04-13 19:25:25 +0000

    Final Push

--- Create_table.sql
+++ Create_table.sql
@@ -1,1 +1,19 @@
-drop table retail_credit_risk.credit_schema.data_master;
+CREATE OR REPLACE TABLE retail_loan_applications (
+    application_id VARCHAR PRIMARY KEY,
+    customer_name VARCHAR,
+    age INT,
+    employment_type VARCHAR,
+    years_at_current_job INT,
+    annual_income FLOAT,
+    cibil_score INT,
+    existing_emi_total FLOAT,
+    kyc_status VARCHAR,
+    pan_aadhaar_match BOOLEAN,
+    ip_address_geo_match BOOLEAN,
+    application_velocity INT,
+    
+    -- These will be populated by our Python "Verdict Engine"
+    risk_score FLOAT,
+    fraud_probability FLOAT,
+    decision VARCHAR
+);
